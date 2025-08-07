4a369c37583948c6445af5c6846b373510b05b33
Zdravko Bozinarov <jstmental@yahoo.com>
2025-08-07 11:45:10 +0300

    Initial Commit
    Initial project setup: Spring Boot + Thymeleaf admin portal layout

---
+++ admin-portal/src/test/java/wanderers/ai/admin_portal/AdminPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package wanderers.ai.admin_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
